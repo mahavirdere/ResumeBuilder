fa27aabb7dbbb3f70de05547966a9766f9267639
mahavirdere <50302981+mahavirdere@users.noreply.github.com>
2021-07-28 11:27:06 +0530

    Completed Home fragment Design and View Model Injection via Hilt.

--- app/src/main/java/com/tech/resumebuilder/ui/home/HomeFragment.kt
+++ app/src/main/java/com/tech/resumebuilder/ui/home/HomeFragment.kt
@@ -24,9 +24,33 @@ class HomeFragment : Fragment() {
             savedInstanceState: Bundle?
     ): View? {
         val root = inflater.inflate(R.layout.fragment_home, container, false)
-        val textView: TextView = root.findViewById(R.id.text_home)
-        homeViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
+        val nameTextView: TextView = root.findViewById(R.id.name_textView)
+        val addressTextView: TextView = root.findViewById(R.id.address_textView)
+        val dobTextView: TextView = root.findViewById(R.id.dob_textView)
+        val nationalityTextView: TextView = root.findViewById(R.id.nationality_textView)
+        val hobbiesTextView: TextView = root.findViewById(R.id.hobbies_textView)
+        val maritalStatusTextView: TextView = root.findViewById(R.id.marital_status_textView)
+        val mobNoTextView: TextView = root.findViewById(R.id.mob_no_textView)
+        homeViewModel.name.observe(viewLifecycleOwner, Observer {
+            nameTextView.text = it
+        })
+        homeViewModel.address.observe(viewLifecycleOwner, Observer {
+            addressTextView.text = it
+        })
+        homeViewModel.dob.observe(viewLifecycleOwner, Observer {
+            dobTextView.text = it
+        })
+        homeViewModel.nationality.observe(viewLifecycleOwner, Observer {
+            nationalityTextView.text = it
+        })
+        homeViewModel.hobbies.observe(viewLifecycleOwner, Observer {
+            hobbiesTextView.text = it
+        })
+        homeViewModel.maritalStatus.observe(viewLifecycleOwner, Observer {
+            maritalStatusTextView.text = it
+        })
+        homeViewModel.mobNo.observe(viewLifecycleOwner, Observer {
+            mobNoTextView.text = it
         })
         return root
     }
--- app/src/main/java/com/tech/resumebuilder/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/tech/resumebuilder/ui/home/HomeViewModel.kt
@@ -9,8 +9,38 @@ import javax.inject.Inject
 @HiltViewModel
 class HomeViewModel @Inject constructor(): ViewModel() {
 
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is home Fragment"
+    private val _name = MutableLiveData<String>().apply {
+        value = "Mahavir Anil Dere"
     }
-    val text: LiveData<String> = _text
+    val name: LiveData<String> = _name
+
+    private val _address = MutableLiveData<String>().apply {
+        value = "Aashirwad Heritage, B-808, Near Sane Chowk Chikhali Pune 412114."
+    }
+    val address: LiveData<String> = _address
+
+    private val _dob = MutableLiveData<String>().apply {
+        value = "29/07/2021"
+    }
+    val dob: LiveData<String> = _dob
+
+    private val _nationality = MutableLiveData<String>().apply {
+        value = "Indian"
+    }
+    val nationality: LiveData<String> = _nationality
+
+    private val _hobbies = MutableLiveData<String>().apply {
+        value = "Listen to Music"
+    }
+    val hobbies: LiveData<String> = _hobbies
+
+    private val _maritalStatus = MutableLiveData<String>().apply {
+        value = "Married"
+    }
+    val maritalStatus: LiveData<String> = _maritalStatus
+
+    private val _mobNo = MutableLiveData<String>().apply {
+        value = "+91-9860242163"
+    }
+    val mobNo: LiveData<String> = _mobNo
 }
